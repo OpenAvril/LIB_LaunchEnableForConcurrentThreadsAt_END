809855b0aebccc70571349569d36c43957ed4f6e
OpenAvril <jasper.assembly@icloud.com>
2026-03-16 17:07:53 +1000

    BRAVO_RELEASE_2026_02_6_1707.

--- LIB_LaunchEnableForConcurrentThreadsAt_END.h
+++ LIB_LaunchEnableForConcurrentThreadsAt_END.h
@@ -3,33 +3,34 @@
 #else
 #define LAUNCHENABLEFORCONCURRENTTHREADSATEND_API __declspec(dllimport)
 #endif
-
-namespace OpenAvrilCLIBLaunchEnableForConcurrentThreadsAtEND
+extern "C"
 {
-	class LAUNCHENABLEFORCONCURRENTTHREADSATEND_API CLIBLaunchEnableForConcurrentThreadsAtEND 
+	namespace OpenAvrilCLIBLaunchEnableForConcurrentThreadsAtEND
 	{
+		class LAUNCHENABLEFORCONCURRENTTHREADSATEND_API CLIBLaunchEnableForConcurrentThreadsAtEND
+		{
 // public.
 	// constructor.
-		CLIBLaunchEnableForConcurrentThreadsAtEND();
+			CLIBLaunchEnableForConcurrentThreadsAtEND();
 
 	// destructor.
-		virtual ~CLIBLaunchEnableForConcurrentThreadsAtEND();
+			virtual ~CLIBLaunchEnableForConcurrentThreadsAtEND();
 	// dynamic.
 		// get.
 		// set.
 	// static.
-		static void* generate_Program();
-		static void terminate_Progaram();
-		static void request_Wait_launch(void* obj, unsigned char* bytes);
-		static void thread_End(void* obj, unsigned char* bytes);
+			static void* generate_Program();
+			static void terminate_Progaram();
+			static void request_Wait_launch(void* obj, unsigned char* bytes);
+			static void thread_End(void* obj, unsigned char* bytes);
 		// get.
-		static unsigned char* get_coreId_To_launch(void* obj);
-		static bool get_Flag_Active(void* obj);
-		static bool get_Flag_ConcurrentCoreState(void* obj, unsigned char* bytes);
-		static bool get_Flag_Idle(void* obj);
-		static bool get_State_launchBit(void* obj);
+			static unsigned char* get_coreId_To_launch(void* obj);
+			static bool get_Flag_Active(void* obj);
+			static bool get_Flag_ConcurrentCoreState(void* obj, unsigned char* bytes);
+			static bool get_Flag_Idle(void* obj);
+			static bool get_State_launchBit(void* obj);
 		// set.
-		static void set_Flag_ConcurrentCoreState(void* obj, unsigned char* bytes, bool newValue);
+			static void set_Flag_ConcurrentCoreState(void* obj, unsigned char* bytes, bool newValue);
 
 // private.
 	// dynamic.
@@ -44,13 +45,14 @@ namespace OpenAvrilCLIBLaunchEnableForConcurrentThreadsAtEND
 	// static.
 		// classes.
 			// create.
-		static void stat_CLASS_create_ptr_Framework();
+			static void stat_CLASS_create_ptr_Framework();
 			// get.
-		static LaunchEnableForConcurrentThreadsAt_END_Framework* stat_CLASS_get_ptr_Framework();
+			static LaunchEnableForConcurrentThreadsAt_END_Framework* stat_CLASS_get_ptr_Framework();
 			// set.
 		// registers.
 			// create.
 			// get.
 			// set.
-	};
+		};
+	}
 }
